911b5aa47bea1b660430cbfc3a9f237e903ff82b
kama casta <kamacasta983@gmail.com>
2021-10-02 19:01:14 -0700

    added logic to the mutations.js not yet completed

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,17 @@
+// import gql
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation
+`
+
+export const ADD_USER = gql`
+    mutation
+`
+export const SAVE_BOOK = gql`
+    mutation
+`
+
+export const REMOVE_BOOK = gql`
+    mutation
+`
