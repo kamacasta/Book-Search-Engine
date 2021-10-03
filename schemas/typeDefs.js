924b48bb4c50aa41504a0ed384b72d19b83439b6
kama casta <kamacasta983@gmail.com>
2021-10-02 17:40:34 -0700

    typeDefs.js completed fixed ID

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -4,19 +4,48 @@ const { gql } = require("apollo-server-express");
 // typeDefs start after back ticks
 
 const typeDefs = gql`
-    type {
+    type User {
         _id: ID
         username: String
         email: String
         bookCount: Init
         savedBooks: [Book]
     }
+
     type Book {
         authors: String
+        bookId: ID
         description: String
         bookId: String
         image: String
         link: String
         title: String
     }
-`
+
+    input SavedBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(content: bookData!): User
+        removeBook(bookId: String!): User
+        
+    }
+
+    typer Auth {
+        token: ID!
+        user: User!
+    }
+
+    type Query {
+        me: User
+    }
+`;
+module.exports = typeDefs;
