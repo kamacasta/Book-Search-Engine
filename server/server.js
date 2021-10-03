fdf104a22f15ce1f71504e7e436919b3e6907dcc
kama casta <kamacasta983@gmail.com>
2021-10-02 23:50:50 -0700

    more debuging

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const db = require('./config/connection');
 const { ApolloServer } = require("apollo-server-express")
 const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require("./schemas");
+const { typeDefs, resolvers } = require("../schemas");
 
 
 const app = express();
