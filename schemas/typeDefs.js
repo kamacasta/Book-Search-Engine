e211811776c685f26948ca605b545ae56a2ab079
kama casta <kamacasta983@gmail.com>
2021-10-02 17:21:57 -0700

    types for User and Book completed not completed yet!

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -4,5 +4,19 @@ const { gql } = require("apollo-server-express");
 // typeDefs start after back ticks
 
 const typeDefs = gql`
-
+    type {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Init
+        savedBooks: [Book]
+    }
+    type Book {
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
 `
