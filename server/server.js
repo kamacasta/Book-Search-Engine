a2fb5b1ff56261da2488e5e38747d67159a94e97
kama casta <kamacasta983@gmail.com>
2021-10-02 23:39:02 -0700

    fixed bugs

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const server = ApolloServer({
   context: authMiddleware,
 });
 
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,8 +27,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}`);
+    console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
